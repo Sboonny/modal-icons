7d4bee2d6fc7bdc06fa848506a1d961334bbfaa6
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-04-17 16:30:01 +0200

    Opening and focusing function
    added a function check if dialog is open add focus to it

--- dialog-component/scripts/dialog.js
+++ dialog-component/scripts/dialog.js
@@ -4,6 +4,38 @@
   // add removed event
   // removing loading attribute
 
+  //track opening
+  const openingMutation =  async (mutations, attrObeserver) => {
+    await mutations.reduce(async (animation , mutation) => {
+     await animation;
+     console.log(animation)
+     if(mutation.attributeName === 'open') {
+      const dialog = mutation.target
+      // check if attribute has open in it 
+      // if it doesn't the function do nothing do nothing
+      const isOpen = dialog.hasAttribute('open')
+      if(!isOpen) return
+
+      dialog.removeAttribute('inert')
+
+      // set focus and set fallback if focustarget doesn't exist
+      const focusTarget = dialog.querySelector('[autofocus]')
+       if(focusTarget) {
+         focusTarget.focus()
+       } else {
+         dialog.querySelector('button').focus()
+       }
+
+       dialog.dispatchEvent(dialogOpeningEvent)
+       await animationComplete(dialog)
+       dialog.dispatchEvent(dialogOpeningEvent)
+
+     }
+     }, Promise.resolve())
+  }
+
+  const dialogAttrObserver = new MutationObserver(openingMutation)
+
   export default async function (dialog) {
     dialog.addEventListener('click', lightDismiss)
   }
